2ef8ce9c7cf521e2c2480f71329f3cb12e368e17
aniketbhurle <bhurle.aniket@gmail.com>
2024-08-08 23:55:03 +0530

    updating

---
+++ Access_Modifier_2/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Access_Modifier_2
+{
+
+
+
+}
---
+++ Access_Modifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Access_Modifiers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class AnotherAssemblyClass1
+}
