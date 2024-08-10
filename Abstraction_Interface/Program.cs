fb5926272aa581a5f4e9b43a3a36eaecffa879ba
aniketbhurle <bhurle.aniket@equinor.com>
2024-08-10 15:58:08 +0530

    Abs Inter

---
+++ Abstraction_Interface/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Abstraction_Interface
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("\nAbstraction Example - Small Bank Ex - INTERFACE\n");
+
+			Console.WriteLine("Welcome to Bank - Axis\n");
+
+			IBankServices bank1 = Bankfactory.BankSelect("Axis");
+
+			bank1.BankTransfer();
+			bank1.CheckBalanace();
+			bank1.WithdrawMoney();
+			bank1.ValidateCard();
+
+
+
+
+
+
+			Console.ReadKey();
+		}
+	}
+
+	interface IBankServices
+	{
+		void ValidateCard();
+		void WithdrawMoney();
+		void CheckBalanace();
+		void BankTransfer();
+		void MiniStatement();
+	}
+
+	class Bankfactory
+	{
+		public static IBankServices BankSelect(string BankType)
+		{
+			IBankServices bankServices = null;
+			if (BankType == "Axis")
+			{
+				bankServices = new AXISB();
+			}
+
+			return bankServices;
+		
+		}
+	}
+
+	class AXISB : IBankServices
+	{
+		public void ValidateCard() {
+			Console.WriteLine("\nAxis Bank Validate Card");
+		}
+		public void WithdrawMoney() {
+			Console.WriteLine("\nAxis Bank Validate Card");
+
+		}
+		public void CheckBalanace() {
+			Console.WriteLine("\nAxis Bank Validate Card");
+		}
+		public void BankTransfer() { Console.WriteLine("\nAxis Bank Validate Card"); 
+		}
+		public void MiniStatement() {
+			Console.WriteLine("\nAxis Bank Validate Card");
+		}
+
+	}
+}
